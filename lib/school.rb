94a3935088712b42371a3162fdb97a3e75690d2d
Julian <julianrosenthal@yahoo.com>
2020-11-13 19:05:33 -0700

    commit tree 5fa7b847a94a1011740869848543b85ae0f0dd1a

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
-# code here!
+class School
+    attr_accessor :name, :roster
+    
+    def initialize(name, roster = {})
+        @name = name
+        @roster = roster
+    end
+
+    def add_student(student_name, student_grade)
+        @roster[student_grade] = [] if !@roster.key?(student_grade)
+        @roster[student_grade] << student_name
+    end
+
+    def grade(grade)
+        @roster[grade]
+    end
+    
+    def sort
+        @roster.map {|grade, students| @roster[grade] = students.sort}
+        @roster
+    end
+end
+
